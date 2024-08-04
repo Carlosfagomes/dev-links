334ebf0cfc8bf4bcea549a6133cdf2352dc03dc5
Carlos Gomes <gomes.cfa@gmail.com>
2024-08-04 18:36:45 +1000

    :tada:

---
+++ script.js
@@ -0,0 +1,34 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  //another way is
+
+  //if (html.classList.contains("light")) {
+  //  html.classList.remove("light")
+  //} else {
+  //  html.classList.add("light")
+  //}
+
+  //substituir imagem quando trocar entre dia e noite
+
+  //pegar a tag img
+
+  //const img = document.querrySelector("#profile img")
+
+  //substituir a imagem
+
+  //if(html.classList.contains("light")) {
+
+  //se tiver light mode, adicionar a imagem light
+
+  //img.setAttribute("src", "./assets/avatar-light.png)
+  //} else {
+
+  // se tiver sem light mode, manter a imagem normal
+
+  //img.setAtrribute("src", "./assets/avatar.png")
+
+  
+
+}
